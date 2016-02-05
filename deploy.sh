77dfac9ff5f24ffcdaaf6dd43a2e824d861301c6
Ricardo N Feliciano <FelicianoTech@gmail.com>
2016-02-05 13:58:02 -0800

    Added requirements.txt

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 configure_aws_cli(){
 	aws --version
-	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+	#aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+	#aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 	aws configure set default.region us-east-1
 	aws configure set default.output json
 }
