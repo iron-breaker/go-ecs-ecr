87ab833d67aff6b03a924e541af55f6197f8b3ba
Ricardo N Feliciano <FelicianoTech@gmail.com>
2016-02-08 09:42:39 -0800

    Testing.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import(
 
 func mainHandler() http.HandlerFunc{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello World!!")
+		fmt.Fprintf(w, "Hello World!!!")
 	})
 }
 
