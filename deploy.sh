a7d5cffe7ff9a1ed1e642c053bf467c9815f304c
Ricardo N Feliciano <FelicianoTech@gmail.com>
2016-02-08 11:17:41 -0800

    Re-added curl test.

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,12 @@ JQ="jq --raw-output --exit-status"
 
 configure_aws_cli(){
 	aws --version
-	#aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-	#aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 	aws configure set default.region us-east-1
 	aws configure set default.output json
 }
 
 deploy_cluster() {
 
-    host_port=80
     family="sample-webapp-task-family"
 
     make_task_def
