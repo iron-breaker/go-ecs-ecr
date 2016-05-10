83ca5e0c4ce6ca0011c15f8112c69225c5cf1563
千葉 淳 <chiba.jun@classmethod.jp>
2016-05-10 23:35:30 +0900

    custom nginx

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 # Building on top of Ubuntu 14.04. The best distro around.
-FROM ubuntu:14.04
+FROM nginx
+COPY static-html-directory /usr/share/nginx/html
 
-COPY ./go-ecs-ecr /opt/
 EXPOSE 8080
-
-ENTRYPOINT ["/opt/go-ecs-ecr"]
--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,48 @@
 #!/usr/bin/env bash
 
+# valiabls
+AWS_DEFAULT_REGION=us-east-1
+AWS_ECS_FAMIRY=nginx-sample-webapp
+AWS_ECS_CLUSTER_NAME=sample-webapp-cluster
+AWS_ECS_SERVICE_NAME=sample-webapp-service
+
+# Create Task Definition
+make_task_def(){
+	task_template='[
+		{
+			"name": "nginx-sample-webapp",
+			"image": "%s.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/nginx-sample-webapp:%s",
+			"essential": true,
+			"memory": 200,
+			"cpu": 10,
+			"portMappings": [
+				{
+					"containerPort": 8080,
+					"hostPort": 80
+				}
+			]
+		}
+	]'
+
+	task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CIRCLE_SHA1)
+}
+
 # more bash-friendly output for jq
 JQ="jq --raw-output --exit-status"
 
 configure_aws_cli(){
 	aws --version
-	aws configure set default.region us-east-1
+	aws configure set default.region ${AWS_DEFAULT_REGION}
 	aws configure set default.output json
 }
 
 deploy_cluster() {
 
-    family="sample-webapp-task-family"
+    family=${AWS_ECS_FAMIRY}
 
     make_task_def
     register_definition
-    if [[ $(aws ecs update-service --cluster sample-webapp-cluster --service sample-webapp-service --task-definition $revision | \
+    if [[ $(aws ecs update-service --cluster ${AWS_ECS_CLUSTER_NAME} --service ${AWS_ECS_SERVICE_NAME} --task-definition $revision | \
                    $JQ '.service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
         return 1
@@ -24,7 +51,7 @@ deploy_cluster() {
     # wait for older revisions to disappear
     # not really necessary, but nice for demos
     for attempt in {1..30}; do
-        if stale=$(aws ecs describe-services --cluster sample-webapp-cluster --services sample-webapp-service | \
+        if stale=$(aws ecs describe-services --cluster ${AWS_ECS_CLUSTER_NAME} --services ${AWS_ECS_SERVICE_NAME} | \
                        $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
             echo "Waiting for stale deployments:"
             echo "$stale"
@@ -38,28 +65,9 @@ deploy_cluster() {
     return 1
 }
 
-make_task_def(){
-	task_template='[
-		{
-			"name": "go-sample-webapp",
-			"image": "%s.dkr.ecr.us-east-1.amazonaws.com/go-sample-webapp:%s",
-			"essential": true,
-			"memory": 200,
-			"cpu": 10,
-			"portMappings": [
-				{
-					"containerPort": 8080,
-					"hostPort": 80
-				}
-			]
-		}
-	]'
-	
-	task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CIRCLE_SHA1)
-}
 
 push_ecr_image(){
-	eval $(aws ecr get-login --region us-east-1)
+	eval $(aws ecr get-login --region ${AWS_DEFAULT_REGION})
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
 }
 
